09d6abe9ec4531d838e9e68242c583ce402efa26
Raul <rauliker04@gmail.com.com>
2024-10-11 19:16:22 +0200

    v16

--- Actividades clase/flutter_dialogos_1/lib/presentation/widgets/widgets.dart
+++
@@ -1,3 +0,0 @@
-export 'dialogo_personalizado_widget.dart';
-export 'dialogo_alerta_widget.dart';
-export 'dialogo_simple_widget.dart';
---
+++ Actividades clase/flutter_dialogs_1/lib/presentation/screens/home_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dialogs_1/presentation/widgets/robot_form.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const FormRobot();
+  }
+}

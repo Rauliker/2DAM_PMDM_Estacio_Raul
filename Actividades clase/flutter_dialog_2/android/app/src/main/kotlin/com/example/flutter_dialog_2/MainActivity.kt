4657838e393d787bf977849509b28c1235ffebcf
Raul <rauliker04@gmail.com.com>
2024-10-11 20:26:10 +0200

    v18

---
+++ Actividades clase/flutter_dialog_2/android/app/src/main/kotlin/com/example/flutter_dialog_2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_dialog_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ Actividades clase/flutter_dialog_2/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'presentations/screens/registrar_screens.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Registro de Usuario',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const RegisterScreen(),
+    );
+  }
+}
